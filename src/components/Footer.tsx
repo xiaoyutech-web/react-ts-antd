720a484ad58c4edd5afeb79e398625dc3abb7368
caizhibiao <caizhibiao@cmhi.chinamobile.com>
2021-08-23 18:57:32 +0800

    OCT 完成首页ui修改

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,4 @@
  /* 描述: 底部footer模板
- *  作者: Jack Chen
- *  日期: 2020-08-02
  */
 
 import * as React from 'react';
@@ -12,7 +10,7 @@ export default class Footer extends React.Component {
             <div className="footer-container">
                 <div className="footer">
                     <div className="copyright">
-                        Copyright@2020-2025 微信公众号{'<懒人码农>'} 湘ICP备19016532号-1
+                        Copyright@2021 中移（杭州）信息技术有限公司
                     </div>
                 </div>
             </div>
